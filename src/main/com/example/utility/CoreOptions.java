8305c542fcf087c823c8d16e8190e7d80ae5ae10
SallyLin <sally1028f@gmail.com>
2016-03-11 15:24:18 +0800

    Modify ADB path in CoreOptions

--- src/main/com/example/utility/CoreOptions.java
+++ src/main/com/example/utility/CoreOptions.java
@@ -3,7 +3,7 @@ package main.com.example.utility;
 public class CoreOptions {
 	public final static String TEST_SCRIPT_DIR = "C:\\Users\\User\\PycharmProjects\\MobileEvernoteTest";
 	public final static String ANDROID_HOME = System.getenv("ANDROID_HOME");
-	public final static String ADB = "/Users/adt-bundle-mac-x86_64-20140702/sdk/platform-tools/adb";
+	public final static String ADB = ANDROID_HOME + "\\platform-tools\\adb.exe";
 	public final static String PYTHON_HOME = System.getenv("PYTHON_HOME");
 	public final static String UPLOAD_DIRECTORY = "D:\\Thesis\\UploadSpace";
 }
