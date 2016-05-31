7b849dd0515393ca20f10bfecb96984090c2abd5
Kevin Ho <kwite2002@gmail.com>
2016-05-31 15:54:16 +0800

    implement pair

--- src/main/com/example/entity/Pair.java
+++ src/main/com/example/entity/Pair.java
@@ -3,20 +3,24 @@ package main.com.example.entity;
 public class Pair {
 	private boolean testComplete = false;
 	private Device phone, wear;
-	
-	public Pair(Device phone, Device wear){
+
+	public Pair(Device phone, Device wear) {
 		this.phone = phone;
 		this.wear = wear;
+	}
+
+	public void setTestComplete(boolean complete) {
+		this.testComplete = complete;
 	}
 
 	public boolean isTestComplete() {
 		return this.testComplete;
 	}
-	
+
 	public Device getPhone() {
 		return this.phone;
 	}
-	
+
 	public Device getWear() {
 		return this.wear;
 	}
