c20b1c70570f82c65807e289359061f8c26466e0
Kevin Ho <kwite2002@gmail.com>
2016-05-31 21:52:06 +0800

    add pair test case

--- src/test/main/com/example/entity/PairTest.java
+++ src/test/main/com/example/entity/PairTest.java
@@ -11,39 +11,59 @@ import main.com.example.entity.Device;
 import main.com.example.entity.Pair;
 
 public class PairTest {
-	private Pair pairTest;
+	private Pair pair;
 	private Device phone, wear;
 	
 	@Before
 	public void setUp() {
 		this.phone = new Device("12345", "test", "nosdcard");
 		this.wear = new Device("54321", "test", "nosdcard, wear");
-		this.pairTest = new Pair(phone, wear);
+		this.pair = new Pair(phone, wear);
+	}
+	
+	@Test
+	public void testSetTestCompleteFalse() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		boolean isComplete = false;
+		this.pair.setTestComplete(isComplete);
+		Field isCompleteField = Pair.class.getDeclaredField("testComplete");
+		isCompleteField.setAccessible(true);
+		boolean actual = (boolean) isCompleteField.get(this.pair);
+		assertFalse(actual);
+	}
+	
+	@Test
+	public void testSetTestCompleteTrue() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		boolean isComplete = true;
+		this.pair.setTestComplete(isComplete);
+		Field isCompleteField = Pair.class.getDeclaredField("testComplete");
+		isCompleteField.setAccessible(true);
+		boolean actual = (boolean) isCompleteField.get(this.pair);
+		assertTrue(actual);
 	}
 
 	@Test
 	public void testIsCompleteFalse() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
 		Field isComplete = Pair.class.getDeclaredField("testComplete");
 		isComplete.setAccessible(true);
-		isComplete.set(this.pairTest, false);
-		assertFalse(this.pairTest.isTestComplete());
+		isComplete.set(this.pair, false);
+		assertFalse(this.pair.isTestComplete());
 	}
 	
 	@Test
 	public void testIsCompleteTrue() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
 		Field isComplete = Pair.class.getDeclaredField("testComplete");
 		isComplete.setAccessible(true);
-		isComplete.set(this.pairTest, true);
-		assertTrue(this.pairTest.isTestComplete());
+		isComplete.set(this.pair, true);
+		assertTrue(this.pair.isTestComplete());
 	}
 	
 	@Test
 	public void testGetPhone() {
-		assertEquals(this.phone, this.pairTest.getPhone());
+		assertEquals(this.phone, this.pair.getPhone());
 	}
 	
 	@Test
 	public void testGetWear() {
-		assertEquals(this.wear, this.pairTest.getWear());
+		assertEquals(this.wear, this.pair.getWear());
 	}
 }
